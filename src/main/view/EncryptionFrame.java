11310e9629b2cd0c6d44854024b2859ef0e4b503
Kirill Kiselev <kirill.v.kiselev@yandex.ru>
2015-11-01 20:54:44 +0300

    Refactored

--- src/main/view/EncryptionFrame.java
+++ src/main/view/EncryptionFrame.java
@@ -14,6 +14,9 @@ public class EncryptionFrame extends JFrame {
     public final static String EMPTY_INPUT_MESSAGE = "Input is empty. Enter phrase to cipher.";
     public final static String WRONG_INPUT_MESSAGE = "Input is not binary. 0 or 1 are only allowed.";
     public final static String ALERT = "Alert";
+    public final static String EMPTY_ENCRYPTED_PHRASE_MESSAGE =
+            "Encrypted phrase is empty. Click GENERATE GAMMA, then input binary number, then click ENCRYPT";
+
 
     private JPanel basePanel;
     private JButton generateGammaButton;
@@ -43,13 +46,14 @@ public class EncryptionFrame extends JFrame {
     }
 
     private void generateGamma(){
+        clearAllExceptInput();
         EncryptionProtocol.generateGamma();
         String gamma = Arrays.toString(EncryptionProtocol.gamma);
         gammaTextField.setText(gamma);
     }
 
     private void encrypt(){
-        clearAllExceptInput();
+        //clearAllExceptInput();
         String input = inputTextField.getText();
 
         if (input.isEmpty()) {
@@ -68,6 +72,14 @@ public class EncryptionFrame extends JFrame {
     }
 
     private void decrypt(){
+        String cipheredPhrase = encryptedPhraseTextField.getText();
+
+        if(cipheredPhrase.isEmpty()){
+            JOptionPane.showMessageDialog(null, EMPTY_ENCRYPTED_PHRASE_MESSAGE, ALERT, JOptionPane.INFORMATION_MESSAGE);
+
+            return;
+        }
+
         String encrypted = encryptedPhraseTextField.getText();
         encrypted = StringUtils.cleanString(encrypted);
         String decrypted = Decryptor.decrypt(encrypted);
@@ -77,12 +89,13 @@ public class EncryptionFrame extends JFrame {
     private void clearAllTextFields() {
         clearAllExceptInput();
         inputTextField.setText("");
+        gammaTextField.setText("");
     }
 
     private void clearAllExceptInput() {
         encryptedPhraseTextField.setText("");
         decryptedPhraseTextField.setText("");
-        gammaTextField.setText("");
+
         inputTextField.requestFocus();
     }
 }
